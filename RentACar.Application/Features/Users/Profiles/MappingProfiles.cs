8a908b863c19accfd281daf8e11587569d27f203
Mehmet Can ÜNALDI <mehmetcanunaldi@gmail.com>
2024-04-03 04:40:59 +0300

    SET4 - 13. Code Generator Kullanımı Öncesi

---
+++ RentACar.Application/Features/Users/Profiles/MappingProfiles.cs
@@ -0,0 +1,13 @@
+﻿using Application.Features.Users.Commands.Create;
+using AutoMapper;
+using Core.Security.Entities;
+
+namespace RentACar.Application.Features.Users.Profiles;
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<User, CreateUserCommand>().ReverseMap();
+        CreateMap<User, CreatedUserResponse>().ReverseMap();
+    }
+}
